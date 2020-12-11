7822182dd936e853af84c3ef247b38c7faebfb56
name-here <felixjyancey@mac.com>
2020-12-11 01:00:48 -0500

    Reorganized some code, added single-thread mode, since HTML5 in Godot 3.2.3 doesn't support threads

--- AutoLoad/AsyncLoader.gd
+++ AutoLoad/AsyncLoader.gd
@@ -14,7 +14,16 @@ class LoadRequest:
 		progress_callback = set_progress_callback
 
 
+# Changing this during runtime could break request_queue
+# (single-thread mode doesn't use the mutexes,
+# and thread(s) will still be running)
+var use_threads := true
+
 const thread_count = 1
+
+#These are only used in single-thread mode
+var current_request:LoadRequest = null
+var loader:ResourceInteractiveLoader
 
 var request_queue:Array
 onready var request_queue_mutex:Mutex = Mutex.new()
@@ -27,22 +36,65 @@ onready var stop_mutex:Mutex = Mutex.new()
 
 
 func _ready() -> void:
-	for i in thread_count:
-		threads.append( Thread.new() )
-		threads[i].start(self, "_loader_thread")
+	if OS.can_use_threads():
+		for i in thread_count:
+			threads.append( Thread.new() )
+			threads[i].start(self, "_loader_thread")
+	else:
+		use_threads = false
 
 
 func load(request:LoadRequest, high_priority:bool = false) -> void:
 	if ResourceLoader.has_cached(request.path):
-		call_deferred( "_callback_complete", request, ResourceLoader.load(request.path) )
+		_callback_complete(request, ResourceLoader.load(request.path))
 		return
-	request_queue_mutex.lock()
-	if high_priority:
-		request_queue.push_front(request)
+	
+	if use_threads:
+		request_queue_mutex.lock()
+		if high_priority:
+			request_queue.push_front(request)
+		else:
+			request_queue.append(request)
+		request_queue_mutex.unlock()
+		semaphore.post()
 	else:
-		request_queue.append(request)
-	request_queue_mutex.unlock()
-	semaphore.post()
+		if high_priority:
+			request_queue.push_front(request)
+		else:
+			request_queue.append(request)
+
+
+var target_frame_time:int = 1000/60#TODO: Move these two to the top of the file<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+var last_ticks:float = 0#Needed because delta doesn't seem to work right sometimes (in 3.2.3)
+var load_error:int
+func _process(_delta) -> void:#Current limitation: this (single-thread mode) can load at most 1 resource per _process() call
+	if not use_threads:
+		if (not current_request) and request_queue.size() > 0:
+			current_request = request_queue.pop_front()
+			loader = ResourceLoader.load_interactive(
+				current_request.path, current_request.type_hint)
+			if loader == null:
+				_callback_complete(current_request, ResourceLoader.load(current_request.path))
+				current_request = null
+				return
+		
+		if current_request:
+			load_error = loader.poll()
+			while OS.get_ticks_msec() - last_ticks < target_frame_time - 1:
+				if load_error == OK:
+					load_error = loader.poll()
+				else:
+					if load_error == ERR_FILE_EOF:
+						_callback_complete(current_request, loader.get_resource())
+						current_request = null
+					else:
+						push_error("Loading failed with error %i"%load_error)
+					break
+			if load_error == OK:
+				_callback_progress(current_request,
+					100.0 * loader.get_stage() / loader.get_stage_count())
+	
+	last_ticks = OS.get_ticks_msec()
 
 
 func _loader_thread(data) -> void:
@@ -67,14 +119,14 @@ func _loader_thread(data) -> void:
 			continue
 		err = loader.poll()
 		while true:
-			if err == ERR_FILE_EOF:
-				call_deferred( "_callback_complete", request, loader.get_resource() )
-				break
-			elif err == OK:
+			if err == OK:
 				_callback_progress(request, 100.0 * loader.get_stage() / loader.get_stage_count())
 				err = loader.poll()
 			else:
-				call_deferred("push_error", err)
+				if err == ERR_FILE_EOF:
+					_callback_complete( request, loader.get_resource())
+				else:
+					call_deferred("push_error", err)
 				break
 
 func _callback_progress(request:LoadRequest, percent:float) -> void:
@@ -82,9 +134,10 @@ func _callback_progress(request:LoadRequest, percent:float) -> void:
 		request.progress_callback.call_func(percent)
 
 func _callback_complete(request:LoadRequest, resource) -> void:
-	call_deferred("_callback_progress", request, 100.0)
+	_callback_progress( request, 100.0)
 	if request.completion_callback.is_valid():
 		request.completion_callback.call_func(resource)
+
 
 func _exit_tree() -> void:
 	_set_stop_threads(true)
